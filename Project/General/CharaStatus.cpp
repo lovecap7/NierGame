dc4825d1c12a2860f715b02af9f30ef15329ffbd
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2025-10-11 01:52:24 +0900

    up

--- Project/General/CharaStatus.cpp
+++ Project/General/CharaStatus.cpp
@@ -1,7 +1,15 @@
 #include "CharaStatus.h"
+#include "Math/MathSub.h"
+
+namespace
+{
+	constexpr int kMaxStatus = 9999;
+	constexpr int kMinStatus = 1;
+}
 
 CharaStatus::CharaStatus():
-	m_hp(0),
+	m_maxHp(0),
+	m_nowHp(0),
 	m_at(0),
 	m_df(0),
 	m_ms(0.0f)
@@ -10,4 +18,11 @@ CharaStatus::CharaStatus():
 
 CharaStatus::~CharaStatus()
 {
+}
+
+void CharaStatus::OnDamage(int power, int at)
+{
+	int atk = MathSub::ClampInt(at, kMinStatus, kMaxStatus);
+	int df = MathSub::ClampInt(m_df, kMinStatus, kMaxStatus);
+	int damage = power * (atk * atk) / (atk + df);
 }
--- Project/General/CharaStatus.h
+++ Project/General/CharaStatus.h
@@ -7,13 +7,20 @@ public:
 	~CharaStatus();
 	//体力
 	int GetMaxHP()const { return m_maxHp; };
+	void SetMaxHP(int maxHP) { m_maxHp = maxHP; };
 	int GetNowHP()const { return m_nowHp; };
+	void SetNowHP(int nowHP) { m_nowHp = nowHP; };
 	//攻撃力
 	int GetAT()const { return m_at; };
+	void SetAT(int at) { m_at = at; };
 	//防御力
 	int GetDF()const { return m_df; };
+	void SetDF(float df) { m_df = df; };
 	//移動速度
 	float GetMS()const { return m_ms; };
+	void SetMS(float ms) { m_ms = ms; };
+	//ダメージ
+	void OnDamage(int power,int at);
 private:
 	//ステータス
 	int m_maxHp;	//最大体力
