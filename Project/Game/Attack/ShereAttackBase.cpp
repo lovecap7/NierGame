e34bae04a67b127287b46e7411fdf4ef16329884
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2025-10-17 14:59:32 +0900

    yo

---
+++ Project/Game/Attack/ShereAttackBase.cpp
@@ -0,0 +1,17 @@
+#include "ShereAttackBase.h"
+#include "../../General/Collision/Rigidbody.h"
+#include "../../General/Collision/SphereCollider.h"
+SphereAttackBase::SphereAttackBase(std::shared_ptr<AttackData> attackData, std::weak_ptr<CharacterBase> pOwner) :
+	AttackBase(Shape::Sphere, attackData, pOwner)
+{
+}
+
+void SphereAttackBase::SetPos(const Vector3& pos)
+{
+	m_rb->m_pos = pos;
+}
+
+void SphereAttackBase::SetRadius(float radius)
+{
+	std::dynamic_pointer_cast<SphereCollider>(m_collisionData)->SetRadius(radius);
+}
---
+++ Project/Game/Attack/ShereAttackBase.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "AttackBase.h"
+class Actor;
+class CharacterStateBase;
+class SphereAttackBase abstract :
+	public AttackBase
+{
+public:
+	SphereAttackBase(std::shared_ptr<AttackData> attackData, std::weak_ptr<CharacterBase> pOwner);
+	virtual ~SphereAttackBase() {};
+	//’†“_
+	void SetPos(const Vector3& pos);
+	//”¼Œa
+	void SetRadius(float radius);
+};
+
